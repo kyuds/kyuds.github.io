8ed2f279398820663746cbd871050d587368aeed
Daniel Shin <kyuseung1016@gmail.com>
2024-02-08 10:37:09 +0900

    .

--- assets/js/spiral.js
+++
@@ -1,9 +0,0 @@
-let images = Array.from(document.querySelectorAll(".section"));
-
-window.addEventListener("DOMContentLoaded", (e) => {
-    images.forEach((image, idx) => {
-        setTimeout(() => {
-            image.classList.add("active");
-        }, idx * 100)
-    })
-});
